fd4487e5442f03829a9790d70462ba1967e208c4
DESKTOP-DGQ8ADJ\miroku <1>
2023-01-19 11:58:10 +0900

    コメント追加

--- Background.cpp
+++ Background.cpp
@@ -12,7 +12,7 @@ Background::~Background()
 bool Background::Start()
 {
 	m_modelRender.Init("Assets/modelData/background/dungeon01.tkm");
-	
+	//test
 	//m_modelRender.SetPosition(m_position + Vector3(0.0f,30.0f,0.0f));
 	m_modelRender.SetRotation(m_rotation);
 	m_modelRender.SetScale(m_scale);
